716bc25d363e1f8d15ed4e64636ec94b012d961a
Nworlu <jehoshaphategbe1@gmail.com>
2024-03-22 20:36:16 -0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig, passthroughImageService } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), mdx()],
+  image: {
+    service: passthroughImageService()
+  }
+});
