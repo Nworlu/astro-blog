b0b457f8738a1e848028e8d385557213382b70ae
Nworlu <jehoshaphategbe1@gmail.com>
2024-03-22 21:21:38 -0700

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   image: {
     service: passthroughImageService()
-  }
+  },
+  output: "server",
+  adapter: netlify()
 });
